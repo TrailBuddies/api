74d8d54500fc1e8fd22effd74825c63445084d28
matievisthekat <matievisthekat@gmail.com>
2021-11-02 10:55:52 +0200

    update uuid config (still doesnt work though)

--- db/migrate/20211101191008_create_users.rb
+++ db/migrate/20211101191008_create_users.rb
@@ -1,9 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :users do |t|
-      t.string :username
-      t.string :password_hash
-      t.string :email
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    create_table :users, id: :uuid, default: 'gen_random_uuid()' do |t|
+      t.string :username, null: false
+      t.string :password, null: false
+      t.string :email, null: false
       t.float :location_lat
       t.float :location_lng
 
--- db/migrate/20211101191313_create_hikes.rb
+++ db/migrate/20211101191313_create_hikes.rb
@@ -2,8 +2,8 @@ class CreateHikes < ActiveRecord::Migration[6.1]
   def change
     create_table :hikes do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :trail
-      t.timestamp :scheduled_for
+      t.string :trail, null: false
+      t.timestamp :scheduled_for, null: false
 
       t.timestamps
     end
