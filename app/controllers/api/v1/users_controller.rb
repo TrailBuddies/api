272f64574fb66ccc0d26071a8ce91bcb9337fcba
matievisthekat <matievisthekat@gmail.com>
2021-11-04 20:46:34 +0200

    require user params to create user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::UsersController < ApplicationController
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: { error: 'Failed to create user' }, status: 400
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user = User.find(params[:id])
+    if @user
+      @user.destroy
+      render json: { success: true }, status: 200
+    else
+      render json: { error: 'User does not exist' }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.require([:email, :password, :username])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_081034) do
+ActiveRecord::Schema.define(version: 2021_11_04_183307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "hikes", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "trail"
-    t.datetime "scheduled_for"
+    t.string "trail", null: false
+    t.datetime "scheduled_for", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_hikes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password_hash"
-    t.string "email"
+    t.string "username", null: false
+    t.string "email", null: false
     t.float "location_lat"
     t.float "location_lng"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password", null: false
   end
 
   add_foreign_key "hikes", "users"
