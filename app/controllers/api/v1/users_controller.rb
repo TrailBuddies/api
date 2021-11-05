eb8e88fa7fb821c7f8861313996c50a640618d6a
matievisthekat <matievisthekat@gmail.com>
2021-11-05 16:26:27 +0200

    require and permit params to create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -35,6 +35,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require([:email, :password, :username])
+    params.require(:user).permit(:username, :email, :password)
   end
 end
