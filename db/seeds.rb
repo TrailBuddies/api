eeccf3d0e0ac88006ba01f78336b3f3c8dc2e5a5
matievisthekat <matievisthekat@gmail.com>
2021-11-07 18:42:37 +0200

    add default user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'bcrypt'
+
+User.create(
+  username: 'matievisthekat',
+  password: BCrypt::Password.create(ENV['PASSWORD']),
+  email: ENV['EMAIL'],
+  admin: true,
+  verified: true,
+)
