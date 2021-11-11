18794573ec265c5d369fca9487f38b0a135d13c9
matievisthekat <matievisthekat@gmail.com>
2021-11-11 15:19:01 +0200

    rename password column to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_170254) do
+ActiveRecord::Schema.define(version: 2021_11_11_131714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_07_170254) do
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "username", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'bcrypt'
 
 User.create(
   username: 'matievisthekat',
-  password: BCrypt::Password.create(ENV['PASSWORD']),
+  password_digest: BCrypt::Password.create(ENV['PASSWORD']),
   email: ENV['EMAIL'],
   admin: true,
   verified: 'complete',
