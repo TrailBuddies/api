568d2c794f1d101596374fd85b3829994dfee73e
matievisthekat <matievisthekat@gmail.com>
2021-11-11 15:31:19 +0200

    rename password_digest back to password

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'bcrypt'
 
 User.create(
   username: 'matievisthekat',
-  password_digest: BCrypt::Password.create(ENV['PASSWORD']),
+  password: BCrypt::Password.create(ENV['PASSWORD']),
   email: ENV['EMAIL'],
   admin: true,
   verified: 'complete',
