6ce32600db1429bb7d1d5b6c965c07e5173073c3
matievisthekat <matievisthekat@gmail.com>
2022-02-06 15:39:16 +0200

    move user routes above /:id path
    so that users/:id doesn't handle all user routes (eg 'users/login', 'users/me', etc...)

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'users/:id', to: 'users#show', :constraints  => { :id => /[0-z\.]+/ }
       get 'users/me', to: 'users#me'
       post 'users/login', to: 'users#login'
       post 'users/register', to: 'users#register'
       delete 'users/logout', to: 'users#logout'
+      get 'users/:id', to: 'users#show', :constraints  => { :id => /[0-z\.]+/ }
 
       resources :users
     end
