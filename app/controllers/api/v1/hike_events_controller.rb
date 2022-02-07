c94c1e2754350b09bd65f3a876c4870ea025ed4d
matievisthekat <matievisthekat@gmail.com>
2022-02-07 17:46:51 +0200

    add show and create methods to hike_events

--- app/controllers/api/v1/hike_events_controller.rb
+++ app/controllers/api/v1/hike_events_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::HikeEventsController < ApplicationController
-  before_action :require_token, only: [:mine]
+  before_action :require_token, only: [:mine, :create, :destroy, :update]
 
   def mine
     render json: @user.hike_events
@@ -7,5 +7,27 @@ class Api::V1::HikeEventsController < ApplicationController
 
   def index
     render json: HikeEvent.all
+  end
+
+  def show
+    render json: HikeEvent.find(params[:id])
+  end
+
+  def create
+    render json: HikeEvent.create(create_params)
+  end
+
+  private
+  
+  def create_params
+    params.require(:hike_event).permit(:title, :description, :duration, :lat, :lng, :difficulty, :image_url).tap do |event|
+      event.require(:title)
+      event.require(:description)
+      event.require(:duration)
+      event.require(:lat)
+      event.require(:lng)
+      event.require(:difficulty)
+      event.require(:image_url)
+    end
   end
 end
