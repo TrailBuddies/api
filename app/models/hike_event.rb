693bd06e9234154d67563af0eda98895fd5bba20
matievisthekat <matievisthekat@gmail.com>
2022-02-18 12:01:01 +0200

    generate image of area for hike event

--- app/models/hike_event.rb
+++ app/models/hike_event.rb
@@ -1,3 +1,32 @@
+require 'httparty'
+require 'tempfile'
+
 class HikeEvent < ApplicationRecord
   has_one_attached :image, dependent: :destroy
+  before_create :generate_image
+
+  def generate_image
+    res = HTTParty.get('https://api.tomtom.com/map/1/staticimage', query: {
+      key: ENV['TOMTOM_API_KEY'],
+      layer: 'basic',
+      style: 'main',
+      format: 'png',
+      center: "#{self.lat},#{self.lng}",
+      width: '1024',
+      height: '336',
+      zoom: '12',
+    });
+
+    if res.code == 200
+      Tempfile.create do |tmp|
+        File.open(tmp.path, 'w+b') do |file|
+          file.write(res);
+          file.rewind;
+          file.close;
+        end
+
+        self.image.attach(io: File.open(tmp.path), filename: 'location.png')
+      end
+    end
+  end
 end
