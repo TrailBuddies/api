dd1114960cd7a8f4f5f3ee2a80dbc9eec9ff05ed
matievisthekat <matievisthekat@gmail.com>
2022-03-04 11:52:40 +0200

    add uuid[] users column to hike events
    im not going to reference. ill be a dangerous boy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_091713) do
+ActiveRecord::Schema.define(version: 2022_03_04_094328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_03_04_091713) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.uuid "user_id", null: false
+    t.uuid "users", default: [], array: true
     t.index ["user_id"], name: "index_hike_events_on_user_id"
   end
 
