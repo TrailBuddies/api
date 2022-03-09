9a2001ccc151f9581cb4dba6a83018fedfa04974
matievisthekat <matievisthekat@gmail.com>
2022-03-09 17:54:03 +0200

    add notification mailer

---
+++ test/mailers/previews/notifications_mailer_preview.rb
@@ -0,0 +1,14 @@
+# Preview all emails at http://localhost:3000/rails/mailers/notifications_mailer
+class NotificationsMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/notifications_mailer/confirm_email
+  def confirm_email
+    NotificationsMailer.confirm_email
+  end
+
+  # Preview this email at http://localhost:3000/rails/mailers/notifications_mailer/forgot_password
+  def forgot_password
+    NotificationsMailer.forgot_password
+  end
+
+end
