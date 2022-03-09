8c0a7fa3af7339b52f47f8eff5416c0e6788c9b1
matievisthekat <matievisthekat@gmail.com>
2022-03-09 20:23:17 +0200

    use test user when previewing mailer view

--- test/mailers/previews/notifications_mailer_preview.rb
+++ test/mailers/previews/notifications_mailer_preview.rb
@@ -3,7 +3,7 @@ class NotificationsMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/notifications_mailer/confirm_email
   def confirm_email
-    NotificationsMailer.confirm_email
+    NotificationsMailer.confirm_email(User.find_by(username: 'test'))
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/notifications_mailer/forgot_password
