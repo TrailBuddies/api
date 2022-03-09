4c4181a1304221fdcd021837d8a5435e07acfc45
matievisthekat <matievisthekat@gmail.com>
2022-03-09 21:14:56 +0200

    allow different locales for confirmation email

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -8,7 +8,7 @@ class NotificationsMailer < ApplicationMailer
   def confirm_email(user)
     @link = user.confirm_email_key.url
     @user = user
-    mail(to: user.email, subject: I18n.t('Hey there! Remember to confirm your email for TrailBuddies!'))
+    mail(to: user.email, subject: "#{I18n.t('greeting.informal.short')}, #{I18n.t('notifications_mailer.confirm_email.subject')}!")
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
