a3bc01d3881168047352450254109624c156d81a
matievisthekat <matievisthekat@gmail.com>
2022-05-04 14:58:45 +0200

    remove weird tests

--- test/mailers/notifications_mailer_test.rb
+++ test/mailers/notifications_mailer_test.rb
@@ -1,20 +1,19 @@
 require "test_helper"
 
 class NotificationsMailerTest < ActionMailer::TestCase
-  test "confirm_email" do
-    mail = NotificationsMailer.confirm_email
-    assert_equal "Confirm email", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
+  # test "confirm_email" do
+  #   mail = NotificationsMailer.confirm_email
+  #   assert_equal "Confirm email", mail.subject
+  #   assert_equal ["to@example.org"], mail.to
+  #   assert_equal ["from@example.com"], mail.from
+  #   assert_match "Hi", mail.body.encoded
+  # end
 
-  test "forgot_password" do
-    mail = NotificationsMailer.forgot_password
-    assert_equal "Forgot password", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
-  end
-
+  # test "forgot_password" do
+  #   mail = NotificationsMailer.forgot_password
+  #   assert_equal "Forgot password", mail.subject
+  #   assert_equal ["to@example.org"], mail.to
+  #   assert_equal ["from@example.com"], mail.from
+  #   assert_match "Hi", mail.body.encoded
+  # end
 end
