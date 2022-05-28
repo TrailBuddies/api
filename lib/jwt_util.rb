5e2e1df644d44471fa8b5c92e8a2480ff0255819
matievisthekat <matievisthekat@gmail.com>
2022-05-28 10:54:56 +0200

    create jwt encode and decode util funcs

---
+++ lib/jwt_util.rb
@@ -0,0 +1,28 @@
+module JWTUtil
+  include RSAUtil
+
+  def self::encode(user)
+    JWT.encode(
+      {
+        iss: 'probably digitalocean or some shit',
+        iat: Time.now.to_i,
+        sub: user.id + '.' + Time.now.to_s
+      },
+      OpenSSL::PKey::RSA.new(RSAUtil::Keys::priv, ENV['PASSPHRASE']),
+      'RS256'
+    )
+  end
+
+  def self::decode(token)
+    begin
+      JWT.decode(
+        token,
+        OpenSSL::PKey::RSA.new(RSAUtil::Keys::pub, ENV['PASSPHRASE']),
+        true,
+        { algorithm: 'RS256' }
+      )
+    rescue JWT::DecodeError
+      return nil
+    end
+  end
+end
