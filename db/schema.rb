7eff55273b87028f41166d0bfe2c558a0193e3e5
matievisthekat <matievisthekat@gmail.com>
2022-07-26 15:39:21 +0200

    remove avatar col from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_10_104307) do
+ActiveRecord::Schema.define(version: 2022_07_26_133854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 2022_06_10_104307) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false, null: false
-    t.string "avatar"
     t.boolean "verified", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
