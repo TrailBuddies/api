b048044064eeb1f9da5b850c68c94bb46dbca59f
matievisthekat <matievisthekat@gmail.com>
2022-08-21 21:02:26 +0200

    write confirm_email tests

--- test/mailers/notifications_mailer_test.rb
+++ test/mailers/notifications_mailer_test.rb
@@ -1,13 +1,16 @@
 require "test_helper"
 
 class NotificationsMailerTest < ActionMailer::TestCase
-  # test "confirm_email" do
-  #   mail = NotificationsMailer.confirm_email
-  #   assert_equal "Confirm email", mail.subject
-  #   assert_equal ["to@example.org"], mail.to
-  #   assert_equal ["from@example.com"], mail.from
-  #   assert_match "Hi", mail.body.encoded
-  # end
+  test "confirm_email" do
+    user = users(:matievisthekat)
+    link = confirm_email_keys(:matievisthekat_user_confirm_email_key)
+    mail = NotificationsMailer.confirm_email(user)
+
+    assert_equal "#{I18n.t('greeting.informal.short')}, #{I18n.t('notifications_mailer.confirm_email.subject')}", mail.subject
+    assert_equal [user.email], mail.to
+    assert_nil mail.from.to_s.match(/^\w+\@trailbuddies.club$/)
+    assert_includes mail.body, link.url
+  end
 
   # test "forgot_password" do
   #   mail = NotificationsMailer.forgot_password
