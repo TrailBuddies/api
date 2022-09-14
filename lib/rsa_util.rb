597db86077d0441705504e41738b8260262e42bb
matievisthekat <matievisthekat@gmail.com>
2022-09-14 10:24:41 +0200

    store passphrase in txt file in `config/rsa` dir

--- lib/rsa_util.rb
+++ lib/rsa_util.rb
@@ -1,5 +1,9 @@
 module RSAUtil
   module Keys
+    def self::passphrase
+      return File.read('config/rsa/passphrase.txt').gsub('\n', '')
+    end
+
     def self::priv
       return File.read('config/rsa/private.pem')
     end
